68af10c770d89b7401947d88d9a4af77ec5962ce
Sakari <20642596+sakarie9@users.noreply.github.com>
2024-05-29 22:30:52 +0800

    fix

--- sway-killactive-wrapper.sh
+++ sway-killactive-wrapper.sh
@@ -3,7 +3,7 @@
 # https://wiki.hyprland.org/Configuring/Uncommon-tips--tricks/#minimize-steam-instead-of-killing
 
 # Kill active if hyprctl fails
-active_window_class=$(swaymsg -t get_tree | jq '.. | select(.type?) | select(.focused==true) | .app_id')
+active_window_class=$(swaymsg -t get_tree | jq -r '.. | select(.type?) | select(.focused==true) | .app_id')
 
 if [ "$active_window_class" = "steam" ]; then
   xdotool getactivewindow windowunmap
