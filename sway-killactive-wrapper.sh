50de40954c162bac059396820688fda4d55cca5a
Sakari <20642596+sakarie9@users.noreply.github.com>
2024-12-22 11:41:21 +0800

    sway-killactive-wrapper: add tg rules

--- sway-killactive-wrapper.sh
+++ sway-killactive-wrapper.sh
@@ -5,6 +5,9 @@
 # Kill active if hyprctl fails
 active_window_class=$(swaymsg -t get_tree | jq -r '.. | select(.type?) | select(.focused==true) | .app_id')
 
+TG_APPID="org.telegram.desktop"
+AYU_APPID="com.ayugram.desktop"
+
 case "$active_window_class" in
 "steam")
   xdotool getactivewindow windowunmap
@@ -13,6 +16,14 @@ case "$active_window_class" in
   sudo systemctl stop waydroid-container.service
   ;;
 "popup_term") ;;
+"$TG_APPID")
+  window_id=$(swaymsg -t get_tree | jq --arg appid "$TG_APPID" '.. | select(.app_id? == $appid) | .id')
+  swaymsg "[con_id=$window_id]" move scratchpad
+  ;;
+"$AYU_APPID")
+  window_id=$(swaymsg -t get_tree | jq --arg appid "$AYU_APPID" '.. | select(.app_id? == $appid) | .id')
+  swaymsg "[con_id=$window_id]" move scratchpad
+  ;;
 *)
   swaymsg kill
   ;;
